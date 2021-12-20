29099864c7b28f59f4a9ac43d5375ef8577bddd1
Ray Chan <raymond.v.chan@gmail.com>
2021-12-19 23:46:00 -0800

    Bot fixes on resuming monitoring

--- bot.py
+++ bot.py
@@ -20,15 +20,41 @@ updater = Updater(token=TELEGRAM_TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
 
+def remove_job_if_exists(name: str, context: CallbackContext) -> bool:
+    """Remove job with given name. Returns whether job was removed."""
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
+
+
 def monitor_portfolio_start(update: Update, context: CallbackContext):
-    context.job_queue.start()
-    context.job_queue.run_repeating(summary, 3600, context=update.message.chat_id)
-    context.job_queue.run_repeating(alert_actions, 600, context=update.message.chat_id)
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Monitoring...")
+    # context.job_queue.start()
+    chat_id = update.message.chat_id
+    try:
+        job_removed = remove_job_if_exists(str(chat_id), context)
+
+        context.job_queue.run_repeating(
+            summary, 3600, context=chat_id, name=str(chat_id) + "summary"
+        )
+        context.job_queue.run_repeating(
+            alert_actions, 3600, context=chat_id, name=str(chat_id) + "alert"
+        )
+        text = "Monitoring..."
+        if job_removed:
+            text += " Old job removed"
+        context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+    except Exception as e:
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f"Error: {e}")
 
 
 def monitor_portfolio_stop(update: Update, context: CallbackContext):
-    context.job_queue.stop()
+    # context.job_queue.stop()
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id) + "summary", context)
+    job_removed = remove_job_if_exists(str(chat_id) + "alert", context)
     context.bot.send_message(
         chat_id=update.effective_chat.id, text="Monitoring Stopped..."
     )
